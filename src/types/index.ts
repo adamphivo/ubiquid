3772edd0fe518f47ec5a0007effa6acacd693361
adamphivo <adamphivo@gmail.com>
2023-06-12 05:29:50 +0200

    🎉 - Init

---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+export interface IJob {
+  uuid: string;
+  jobTitle: string;
+  contractType: string;
+  isActive: boolean;
+  remoteWork: string;
+  salary: number;
+  studyLevel: number;
+  currency: string;
+  city: string;
+  company: string;
+  applyMail: string;
+  about: string;
+  startDate: string;
+  publishDate: string;
+}
